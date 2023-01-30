6cf18849cd27ae3074325133512c1dfb5134be55
Andrey Waqued <andrey.w@live.com>
2023-01-30 19:45:28 -0400

    create odds_api script

--- main_copy.py
+++ main_copy.py
@@ -1,15 +1,22 @@
 from sbrscrape import Scoreboard
 
-def averageValues(values):
+
+def averageTotal(values):
     return sum(values)/len(values)
+
+def averageValues(values):
+    totalOdds = 0
+    for value in values:
+        totalOdds += conversor_odds(value)
+    return totalOdds/len(values)
 
 def conversor_odds(oddsAmericanas):
 	oddsDecimal = 0
 	if oddsAmericanas > 0:
 		oddsDecimal = (oddsAmericanas / 100) + 1
 	elif oddsAmericanas < 0:
-		oddsDecimal = (-100 / oddsAmericanas) + 1
-	return round(oddsDecimal, 2)
+		oddsDecimal = (100 / abs(oddsAmericanas)) + 1
+	return oddsDecimal
 
 
 class SbrOddsProvider:
@@ -25,31 +32,32 @@ class SbrOddsProvider:
 		self.sportsbook = sportsbook
 		#print(self.games)
 		for game in self.games:
-			total = 1/conversor_odds(averageValues(game["home_ml"].values())) + 1/conversor_odds(averageValues(game["away_ml"].values()))
-			homeWin = 1/conversor_odds(averageValues(game["home_ml"].values()))/total
-			awayWin = 1/conversor_odds(averageValues(game["away_ml"].values()))/total
-			totalUO = 1/conversor_odds(averageValues(game["under_odds"].values())) + 1/conversor_odds(averageValues(game["over_odds"].values()))
-			underWin = 1/conversor_odds(averageValues(game["under_odds"].values()))/totalUO
-			overWin = 1/conversor_odds(averageValues(game["over_odds"].values()))/totalUO
-			# print("home_ml: (O.A.: " + str(averageValues(game["home_ml"].values())) + ") (O.D.: " + str(conversor_odds(averageValues(game["home_ml"].values()))) + ") WIN% :" + str(homeWin))
-			# print("away_ml: (O.A.: " + str(averageValues(game["away_ml"].values())) + ") (O.D.: " + str(conversor_odds(averageValues(game["away_ml"].values()))) + ") WIN% :" + str(awayWin))
-			# print("total: " + str(averageValues(game["total"].values())))
-			# print("under_odds: (O.A.: " + str(averageValues(game["under_odds"].values())) + ") (O.D.: " + str(conversor_odds(averageValues(game["under_odds"].values()))) + ") WIN% :" + str(underWin))
-			# print("over_odds: (O.A.: " + str(averageValues(game["over_odds"].values())) + ") (O.D.: " + str(conversor_odds(averageValues(game["over_odds"].values()))) + ") WIN% :" + str(overWin))
+			total = 1/averageValues(game["home_ml"].values()) + 1/averageValues(game["away_ml"].values())
+			homeWin = 1/averageValues(game["home_ml"].values())/total
+			awayWin = 1/averageValues(game["away_ml"].values())/total
+			totalUO = 1/averageValues(game["under_odds"].values()) + 1/averageValues(game["over_odds"].values())
+			underWin = 1/averageValues(game["under_odds"].values())/totalUO
+			overWin = 1/averageValues(game["over_odds"].values())/totalUO
 			print(game["home_team"] + " (WIN% " + str(round(homeWin, 2)) + ") (BEOdds " + str(round(1/homeWin,2)) + ") X " + game["away_team"] + " (WIN% " + str(round(awayWin,2)) + ") (BEOdds " + str(round(1/awayWin, 2)) + ")")
-			print("under " + str(round(averageValues(game["total"].values()), 2)) + " (WIN% " + str(round(underWin, 2)) + ") (BEOdds " + str(round(1/underWin,2)) + " / over " + str(round(averageValues(game["total"].values()), 2)) + " (WIN% " + str(round(overWin,2)) + ") (BEOdds " + str(round(1/overWin, 2)) + ")")
+			print("under " + str(round(averageTotal(game["total"].values()), 2)) + " (WIN% " + str(round(underWin, 2)) + ") (BEOdds " + str(round(1/underWin,2)) + " / over " + str(round(averageTotal(game["total"].values()), 2)) + " (WIN% " + str(round(overWin,2)) + ") (BEOdds " + str(round(1/overWin, 2)) + ")")
+			# print("home_ml: (O.D.: " + str(averageValues(game["home_ml"].values())) + ") WIN% :" + str(homeWin))
+			# print(game["home_ml"])
+			# print("away_ml: (O.D.: " + str(averageValues(game["away_ml"].values())) + ") WIN% :" + str(awayWin))
+			# print("total: " + str(averageValues(game["total"].values())))
+			# print("under_odds: (O.D.: " + str(averageValues(game["under_odds"].values())) + ") WIN% :" + str(underWin))
+			# print("over_odds: (O.D.: " + str(averageValues(game["over_odds"].values())) + ") WIN% :" + str(overWin))
 			for provider, oa in game["home_ml"].items():
 				if conversor_odds(oa) * homeWin > 1:
-					print(provider + " home_ml : (O.A. " + str(oa) + ") (O.D. " + str(conversor_odds(oa)) + ") WIN%: " + str(round(homeWin, 2)) + " EV: " + str(round((conversor_odds(oa) * homeWin - 1) * 100, 2)) + "%")
+					print(provider + "* home_ml : (O.D. " + str(conversor_odds(oa)) + ") WIN%: " + str(round(homeWin, 2)) + " EV: " + str(round((conversor_odds(oa) * homeWin - 1) * 100, 2)) + "%")
 			for provider, oa in game["away_ml"].items():
 				if conversor_odds(oa) * awayWin > 1:
-					print(provider + " away_ml : (O.A. " + str(oa) + ") (O.D. " + str(conversor_odds(oa)) + ") WIN%: " + str(round(awayWin, 2)) + " EV: " + str(round((conversor_odds(oa) * awayWin - 1) * 100, 2)) + "%")
+					print(provider + "* away_ml : (O.D. " + str(conversor_odds(oa)) + ") WIN%: " + str(round(awayWin, 2)) + " EV: " + str(round((conversor_odds(oa) * awayWin - 1) * 100, 2)) + "%")
 			for provider, oa in game["under_odds"].items():
 				if conversor_odds(oa) * underWin > 1:
-					print(provider + " under " + str(round(game["total"][provider], 2)) + ": (O.A. " + str(oa) + ") (O.D. " + str(conversor_odds(oa)) + ") WIN%: " + str(round(underWin, 2)) + " EV: " + str(round((conversor_odds(oa) * underWin - 1) * 100, 2)) + "%")
+					print(provider + "* under " + str(game["total"][provider], 2) + ": (O.D. " + str(round(conversor_odds(oa), 2)) + ") WIN%: " + str(round(underWin, 2)) + " EV: " + str(round((conversor_odds(oa) * underWin - 1) * 100, 2)) + "%")
 			for provider, oa in game["over_odds"].items():
 				if conversor_odds(oa) * overWin > 1:
-					print(provider + " over " + str(round(game["total"][provider], 2)) + ": (O.A. " + str(oa) + ") (O.D. " + str(conversor_odds(oa)) + ") WIN%: " + str(round(overWin, 2)) + " EV: " + str(round((conversor_odds(oa) * overWin - 1) * 100, 2)) + "%")
+					print(provider + "* over " + str(game["total"][provider], 2) + ": (O.D. " + str(round(conversor_odds(oa), 2)) + ") WIN%: " + str(round(overWin, 2)) + " EV: " + str(round((conversor_odds(oa) * overWin - 1) * 100, 2)) + "%")
 			print()
 		
-oddsProvider = SbrOddsProvider(sport="NHL")
+oddsProvider = SbrOddsProvider(sport="NBA")
---
+++ odds-api-provider.py
@@ -0,0 +1,56 @@
+import requests
+import xlsxwriter
+import pandas as pd
+import numpy as np
+import openpyxl
+from openpyxl import Workbook, load_workbook
+from openpyxl.styles import Border, Side, Font, Alignment, PatternFill, numbers
+
+class odds_api_provider:
+	def __init__(self, sport='basketball_nba', regions='eu', markets='h2h,totals', odds_format='decimal', date_format='iso'):
+		API_KEY = 'b44c22cbc286679ea1ff4759ec62497e'
+		SPORT = sport #'basketball_nba' # use the sport_key from the /sports endpoint below, or use 'upcoming' to see the next 8 games across all sports
+
+		REGIONS = regions #'eu' # uk | us | eu | au. Multiple can be specified if comma delimited
+
+		MARKETS = markets #'h2h,totals' # h2h | spreads | totals. Multiple can be specified if comma delimited
+
+		ODDS_FORMAT = odds_format #'decimal' # decimal | american
+
+		DATE_FORMAT = date_format #'iso' # iso | unix
+
+		BET_SIZE = 10
+
+		self.odds_response = requests.get(
+			f'https://api.the-odds-api.com/v4/sports/{SPORT}/odds',
+			params={
+				'api_key': 'b44c22cbc286679ea1ff4759ec62497e',
+				'regions': REGIONS,
+				'markets': MARKETS,
+				'oddsFormat': ODDS_FORMAT,
+				'dateFormat': DATE_FORMAT,
+			}
+		).json()
+	def get_odds(self):
+		# odds_response
+		#print(odds_response)
+		#dictionary: [home_team_name + ':' + away_team_name: { home_team: money_line_odds, away_team: money_line_odds }, under_over_odds: {val, under_odds, over_odds}]
+		oddsDict = {}
+		for game in self.odds_response:
+			#print(game["bookmakers"])
+			for bookmaker in game["bookmakers"]:
+				if bookmaker["key"] == "pinnacle":
+					for markets in bookmaker["markets"]:
+						if markets["outcomes"][0]["name"] == "Over":
+							oddsDict[homeTeam + ":" + awayTeam].update({"under_over_odds" : {"total":markets["outcomes"][0]["point"], "under":markets["outcomes"][0]["price"], "over":markets["outcomes"][1]["price"]}})
+							continue
+						else:
+							homeTeam = markets["outcomes"][0]["name"]
+							awayTeam = markets["outcomes"][1]["name"]
+							oddsDict[homeTeam + ":" + awayTeam] = {homeTeam : markets["outcomes"][0]["price"], awayTeam : markets["outcomes"][1]["price"]} #, "under_over_odds" : {"total" : total, "under": under_odds, "over": over_odds} 
+						#print(oddsDict)
+		print(oddsDict)
+					# for odds in markets["outcomes"]:
+					#     print(odds)
+
+odds_api_provider().get_odds()
